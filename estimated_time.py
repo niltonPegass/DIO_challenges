c4d04330f80ab101668b00a8b574e10ebe7bd90a
Nilton Pegas <92957971+niltonPegass@users.noreply.github.com>
2023-07-10 21:22:16 -0300

    Add files via upload

---
+++ estimated_time.py
@@ -0,0 +1,36 @@
+from string import Template
+
+# dicionário com os exemplos fornecidos no desafio
+# usados no output para a resposta
+cadastro_restaurante = {
+    "MCDONALDS": {"nome": "Mc Donalds", "tempoEstimadoEntrega": 10},
+    "KFC": {"nome": "KFC", "tempoEstimadoEntrega": 25},
+    "BURGUERKING": {"nome": "Burguer King", "tempoEstimadoEntrega": 5},
+    "GIRAFFAS": {"nome": "Giraffas", "tempoEstimadoEntrega": 35}
+}
+# leitura dos dados fornecidos para o usuário, e utilizados
+# na condicional abaixo
+nomeRestaurante = input("Digite o nome do restaurante: ")
+tempoEstimadoEntrega = int(input("Digite o tempo de espera: "))
+
+# condicional que testa se o restaurante já existe no cadastro
+# caso exista nem uma tarefa é executada, caso não exista o
+# o os dados do restaurante são adicionados ao dicionário
+if cadastro_restaurante.get(nomeRestaurante.upper().replace(" ", "")):
+  print(f"Já existe o restaurante {nomeRestaurante} cadastrado")
+else:
+  cadastro_restaurante[nomeRestaurante.upper().replace(" ", "")] = {"nome": nomeRestaurante, "tempoEstimadoEntrega": tempoEstimadoEntrega}
+
+# extração dos dados do dicionário para apresentar na resposta
+# para o usuário
+nomeRestaurante = cadastro_restaurante[nomeRestaurante.upper().replace(" ", "")]["nome"]
+tempoEstimadoEntrega = cadastro_restaurante[nomeRestaurante.upper().replace(" ", "")]["tempoEstimadoEntrega"]
+
+# saída do programa com a resposta na tela
+template = Template("O restaurante $nome_restaurante entrega em $tempoEstimadoEntrega minutos")
+mensagem_usuario = template.substitute(nome_restaurante=nomeRestaurante, tempoEstimadoEntrega=tempoEstimadoEntrega)
+print(mensagem_usuario)
+
+# adicionei os métodos .upper().replace(" ", "") para tratar os dados
+# digitados e garantir que na busca não tenha a chance do que o usuário
+# digitou não seja encontrado por diferenças de espaços e caracteres
