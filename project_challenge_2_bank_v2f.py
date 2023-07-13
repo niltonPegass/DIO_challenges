b1553adafbffbbca63e7e5ef48b53ba5502a1c3d
Nilton Pegas <92957971+niltonPegass@users.noreply.github.com>
2023-07-13 17:12:27 -0300

    Update project_challenge_2_bank_v2f.py

--- project_challenge_2_bank_v2f.py
+++ project_challenge_2_bank_v2f.py
@@ -27,3 +27,59 @@ definições e regras:
 
 dica: para vincular um usuário a uma conta filtre a lista de usuários buscando o número do CPF informado para cada usuário da lista - se não encontrar um usuário, não pode criar uma conta (não pode haver contas sem usuários)
 """
+
+# definição de condições e limites
+LIMITE_SAQUES = 3
+numero_saques = 0
+saldo = 200
+limite = 500
+extrato = ""
+
+# mensagens para operações do usuário fora das 
+# condições e limites inicialmente estabelecidos
+excedeu_saldo = f"Você não possui limite para realizar esta operação."
+excedeu_limite = f"Você excedeu o limite diário de R$ {limite:.2f}.\n"
+excedeu_LIMITE_SAQUES = f"Excede o limite de {LIMITE_SAQUES} saques diários.\n"
+operacao_nao_permitida = "Operação não permitida.\n"
+
+def saque(valor_saque, saldo, limite, extrato):
+    print("SAQUE\n")
+
+    acumulador_valor_saque = 0 # inicializador de acumulador dos valores sacados
+    operacoes_realizadas = 0 # inicializador das ações de saque do usuário
+
+    # condições para validação das operações
+    # solicitadas pelo usuário
+    validacao_1 = operacoes_realizadas < LIMITE_SAQUES
+    validacao_2 = valor_saque + acumulador_valor_saque <= limite
+    validacao_3 = saldo > 0
+#   validacao_4 = (saldo - valor_saque) < 0
+
+    if validacao_1 == False:
+        print(f"{excedeu_LIMITE_SAQUES}{operacao_nao_permitida}")
+
+    elif validacao_2 == False:
+        print(f"{excedeu_limite}{operacao_nao_permitida}")
+
+    # validacao_1 - usuário só pode realizar n operações
+    # validacao_2 - usuário só pode realizar R$ x em saques (tanto unitário, quanto somatório)
+    # validacao_3 - usuário só pode realizar saque se tiver saldo acima de R$ 0,00
+    elif validacao_1 and validacao_2 and validacao_3:
+
+        extrato += f"> Saque de R$ {valor_saque:.2f} (-)\n" # adiciona a operação unitária de saque na lista extrato
+        saldo -= valor_saque
+        acumulador_valor_saque += valor_saque
+        operacoes_realizadas += 1
+    
+#   elif validacao_4 == False:
+#       print(f"{excedeu_saldo} {operacao_nao_permitida}")
+
+    print(f"O saque acumulado é: R$ {acumulador_valor_saque:.2f}")
+    print(f"O saldo atual é: R$ {saldo:.2f}")
+
+    return
+
+# parâmetros da função saque
+valor_saque = float(input("Informe o valor do saque: R$ "))
+
+print(saque(valor_saque=valor_saque, saldo=saldo, limite=limite, extrato=extrato))
