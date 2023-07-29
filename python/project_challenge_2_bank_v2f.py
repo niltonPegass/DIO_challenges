bb615c6404f7f107b24bc4c0e59254706f7ff8fc
Nilton Pegas <92957971+niltonPegass@users.noreply.github.com>
2023-07-28 21:11:25 -0300

    Update project_challenge_2_bank_v2f.py

--- python/project_challenge_2_bank_v2f.py
+++ python/project_challenge_2_bank_v2f.py
@@ -2,27 +2,27 @@
 aprimoramento do "challenge_2_bank_operations_1.py"
 utilizando "funções" para otimização
 
-FEITO       - separar em funções (saque, depósito e extrato);
-FEITO       * função saque: argumentos apenas por nome (keyword only) - ex: saldo=saldo, valor=valor;
-FEITO            ~ sugestão de argumentos: saldo, valor, extrato, limite, numero_saques, limite_saques;
-FEITO            ~ sugestão de retorno: saldo, extrato;
-FEITO       * função depósito: argumentos apenas por posição (positional only)
-FEITO           ~ sugestão de argumentos: saldo, valor, extrato;
-FEITO           ~ sugestão de retorno: saldo e extrato;
-FEITO       * função extrato: argumentos por posição e por nome (positional only e keyword only)
-FEITO           ~ argumentos posicionais: saldo;
-FEITO           ~ argumentos nomeados: extrato;
+- separar em funções (saque, depósito e extrato);
+    * função saque: argumentos apenas por nome (keyword only) - ex: saldo=saldo, valor=valor;
+        ~ sugestão de argumentos: saldo, valor, extrato, limite, numero_saques, limite_saques;
+        ~ sugestão de retorno: saldo, extrato;
+    * função depósito: argumentos apenas por posição (positional only)
+        ~ sugestão de argumentos: saldo, valor, extrato;
+        ~ sugestão de retorno: saldo e extrato;
+    * função extrato: argumentos por posição e por nome (positional only e keyword only)
+        ~ argumentos posicionais: saldo;
+        ~ argumentos nomeados: extrato;
     
 - criar duas novas: usuário (cliente); conta corrente (vincular com usuário);
-FEITO       * usuário;
-FEITO           ~ armazenar usuários em uma lista - composto por nome, data de nascimento, CPF e endereço;
-FEITO               ~ endereço é uma string com formato "logradouro, nº - bairro - cidade/sigla estado";
-FEITO               ~ CPF: armazenar somente os números;
-FEITO               ~ NÃO pode ser possível cadastrar dois usuários com CPFs iguais;
-* conta-corrente - composta por agência, número da conta e usuário.
-    ~ o número da conta é sequencial, iniciando em 1;
-    ~ o número da agência é fixo: "0001";
-    ~ o usuário pode ter mais de uma conta, mas uma conta pertence somente a um usuário;
+    * usuário;
+        ~ armazenar usuários em uma lista - composto por nome, data de nascimento, CPF e endereço;
+        ~ endereço é uma string com formato "logradouro, nº - bairro - cidade/sigla estado";
+        ~ CPF: armazenar somente os números;
+        ~ NÃO pode ser possível cadastrar dois usuários com CPFs iguais;
+    * conta-corrente - composta por agência, número da conta e usuário.
+        ~ o número da conta é sequencial, iniciando em 1;
+        ~ o número da agência é fixo: "0001";
+        ~ o usuário pode ter mais de uma conta, mas uma conta pertence somente a um usuário;
 
 dica: para vincular um usuário a uma conta filtre a lista de usuários buscando o número do CPF informado para
 cada usuário da lista - se não encontrar um usuário, não pode criar uma conta (não pode haver contas sem usuários)
@@ -68,53 +68,54 @@ extrato = ""
 
 excedeu_LIMITE_SAQUES = f"[!] EXCEDE O LIMITE DE {LIMITE_SAQUES} SAQUES DIÁRIOS"
 excedeu_limite = f"[!] VOCÊ EXCEDEU O LIMITE DIÁRIO DE R$ {limite:.2f}."
-nao_possui_saldo = "[!] VOCÊ NÃO POSSUI SALDO SUFICIENTE"
-valor_abaixo_zero = "[!] INFORME UM VALOR ACIMA DE R$ 0,00\n"
-saldo_e = ">> O SALDO ATUAL É: R$ "
 operacao_nao_permitida = "[!] OPERAÇÃO NÃO PERMITIDA\n"
 operacao_permitida = "\n>> OPERAÇÃO BEM SUCEDIDA!\n"
+nao_possui_saldo = "[!] VOCÊ NÃO POSSUI SALDO SUFICIENTE"
+valor_abaixo_zero = "[!] INFORME UM VALOR ACIMA DE R$ 0,00\n"
+necessario_cadastro = "[!] É NECESSÁRIO CADASTRAR O USUÁRIO"
+saldo_e = ">> O SALDO ATUAL É: R$ "
 
 def fdeposito(saldo, extrato, valor_deposito, /):
     
-  if valor_deposito >= 0:
-    saldo += valor_deposito
-    extrato += (f"> DEPÓSITO:\tR$ {valor_deposito:.2f} (+)\n")
-    print(f"{operacao_permitida}{saldo_e}{saldo:.2f}")
+    if valor_deposito >= 0:
+        saldo += valor_deposito
+        extrato += (f"> DEPÓSITO:\tR$ {valor_deposito:.2f} (+)\n")
+        print(f"{operacao_permitida}{saldo_e}{saldo:.2f}")
 
-  else:
-    print(f"{valor_abaixo_zero}")
+    else:
+        print(f"{valor_abaixo_zero}")
 
-  return saldo, extrato
+    return saldo, extrato
 def fsaque(*, valor_saque, limite, saldo, extrato):
-  
-  global acumulador_valor_saque
-  global numero_saques
 
-  validacao_1 = (valor_saque + acumulador_valor_saque) <= limite
-  validacao_2 = numero_saques < LIMITE_SAQUES
-  validacao_3 = (saldo - valor_saque) >= 0
-  
-  if validacao_1 == False and validacao_3 == True:
-    print(f"\n{operacao_nao_permitida}{excedeu_limite}\n")
+    global acumulador_valor_saque
+    global numero_saques
 
-  if validacao_2 == False:
-    print(f"\n{operacao_nao_permitida}{excedeu_LIMITE_SAQUES}\n")
+    validacao_1 = (valor_saque + acumulador_valor_saque) <= limite
+    validacao_2 = numero_saques < LIMITE_SAQUES
+    validacao_3 = (saldo - valor_saque) >= 0
 
-  if validacao_3 == False:
-    print(f"\n{operacao_nao_permitida}{nao_possui_saldo}\n")
+    if validacao_1 == False and validacao_3 == True:
+        print(f"\n{operacao_nao_permitida}{excedeu_limite}\n")
 
-  # validacao_1 - só pode realizar R$ X em saques (tanto unitário, quanto somatório)
-  # validacao_2 - só pode realizar n operações "por dia"
-  # validacao_3 - validação de saldo acima de R$ 0,00 ou se saque não deixa saldo negativo
-  elif validacao_1 and validacao_2 and validacao_3:
-    extrato += (f"> SAQUE:\tR$ {valor_saque:.2f} (-)\n")
-    saldo -= valor_saque
-    acumulador_valor_saque += valor_saque
-    numero_saques += 1
+    if validacao_2 == False:
+        print(f"\n{operacao_nao_permitida}{excedeu_LIMITE_SAQUES}\n")
 
-  print(f"{saldo_e}{saldo:.2f}")
+    if validacao_3 == False:
+        print(f"\n{operacao_nao_permitida}{nao_possui_saldo}\n")
 
-  return saldo, extrato
+    # validacao_1 - só pode realizar R$ X em saques (tanto unitário, quanto somatório)
+    # validacao_2 - só pode realizar n operações "por dia"
+    # validacao_3 - validação de saldo acima de R$ 0,00 ou se saque não deixa saldo negativo
+    elif validacao_1 and validacao_2 and validacao_3:
+        extrato += (f"> SAQUE:\tR$ {valor_saque:.2f} (-)\n")
+        saldo -= valor_saque
+        acumulador_valor_saque += valor_saque
+        numero_saques += 1
+
+    print(f"{saldo_e}{saldo:.2f}")
+
+    return saldo, extrato
 def fextrato(saldo, /, *, extrato):
     
     print(f"""
@@ -127,116 +128,112 @@ def fextrato(saldo, /, *, extrato):
 ===============================""")
 def cadastrar_usuario(cpf):
     
-  if cadastro_usuario.get(cpf):
-    print(f"\n>> CLIENTE {cpf} JÁ POSSUI CADASTRO")
-    nome = cadastro_usuario[cpf]["nome"]
-    nascimento = cadastro_usuario[cpf]["nascimento"]
-    print(f"\n>> NOME: {nome}\n>> NASCIMENTO: {nascimento}")
+    if cadastro_usuario.get(cpf):
+        print(f"\n>> CLIENTE {cpf} JÁ POSSUI CADASTRO")
+        nome = cadastro_usuario[cpf]["nome"]
+        nascimento = cadastro_usuario[cpf]["nascimento"]
+        print(f"\n>> NOME: {nome}\n>> NASCIMENTO: {nascimento}")
 
-  else:
-    nome = input("NOME: ").upper()
-    nascimento = input("NASCIMENTO: ")
-    logradouro = input("RUA: ").upper()
-    num_casa = int(input("NÚMERO: "))
-    bairro = input("BAIRRO: ").upper()
-    cidade = input("CIDADE: ").upper()
-    estado = input("ESTADO (SIGLA): ").upper()
-    cadastro_usuario[cpf] = {"nome": nome, "nascimento": nascimento, "logradouro": logradouro, "num_casa": num_casa, "bairro": bairro, "cidade": cidade, "estado": estado}
+    else:
+        nome = input("NOME: ").upper()
+        nascimento = input("NASCIMENTO: ")
+        logradouro = input("RUA: ").upper()
+        num_casa = int(input("NÚMERO: "))
+        bairro = input("BAIRRO: ").upper()
+        cidade = input("CIDADE: ").upper()
+        estado = input("ESTADO (SIGLA): ").upper()
+        cadastro_usuario[cpf] = {"nome": nome, "nascimento": nascimento, "logradouro": logradouro, "num_casa": num_casa, "bairro": bairro, "cidade": cidade, "estado": estado}
 def listar_usuario():
 
-  for usuario in cadastro_usuario:
-    print(f">> NOME: {cadastro_usuario[usuario]['nome']} - CPF: {usuario}\n>> DATA DE NASCIMENTO: {cadastro_usuario[usuario]['nascimento']}\n")
+    for usuario in cadastro_usuario:
+        print(f">> NOME: {cadastro_usuario[usuario]['nome']} - CPF: {usuario}\n>> DATA DE NASCIMENTO: {cadastro_usuario[usuario]['nascimento']}\n")
 def procurar_usuario():
     
-  usuario_procurado = input(">> INFORME O 'CPF': ")
-  cpf = usuario_procurado
-  if cpf in cadastro_usuario:
-    valor_encontrado = cadastro_usuario[cpf]
-    print(f"\n>> USUÁRIO ENCONTRADO\n>> NOME: {valor_encontrado['nome']}\n>> DATA DE NASCIMENTO: {valor_encontrado['nascimento']}")
-  else:
-    print(f"\n>> USUÁRIO NÃO ENCONTRADO\n>> NÃO EXISTE CLIENTE CADASTRADO COM O CPF {cpf}")
+    usuario_procurado = input(">> INFORME O 'CPF': ")
+    cpf = usuario_procurado
+    if cpf in cadastro_usuario:
+        valor_encontrado = cadastro_usuario[cpf]
+        print(f"\n>> USUÁRIO ENCONTRADO\n>> NOME: {valor_encontrado['nome']}\n>> DATA DE NASCIMENTO: {valor_encontrado['nascimento']}")
+    else:
+        print(f"\n>> USUÁRIO NÃO ENCONTRADO\n>> NÃO EXISTE CLIENTE CADASTRADO COM O CPF {cpf}")
 def cadastrar_conta(cpf):
 
-  ultimo_numero = cadastro_conta[cpf]["conta"]
-  print(f"<<< o ultimo numero de conta é: {ultimo_numero} >>>")
+    if cadastro_conta.get(cpf):
+        print(f"\n[!] CLIENTE {cpf} JÁ POSSUI UMA CONTA")
+        nome = cadastro_usuario[cpf]["nome"]
+        nascimento = cadastro_usuario[cpf]["nascimento"]
+        agencia = cadastro_conta[cpf]["agencia"]
+        conta = cadastro_conta[cpf]["conta"]
+        print(f"\n>> NOME: {nome}\n>> NASCIMENTO: {nascimento}\n>> AGÊNCIA: {agencia} // CONTA: {conta}")
+        
+        opcao = input("\n>> DESEJA CADASTRAR UMA NOVA CONTA PARA ESTE USUÁRIO? [S/N]\n>> ").upper()
+        
+        if opcao == "N":
+            return
+        
+        elif opcao == "S":            
+            conta = input("INFORME A CONTA: ")
+            cadastro_conta[cpf] = {"agencia": agencia, "conta": conta, "usuario": cpf}
+        
+    else:
+        if cadastro_usuario.get(cpf):
+            agencia = "0001"
+            conta = input("INFORME A CONTA: ")
+            cadastro_conta[cpf] = {"agencia": agencia, "conta": conta, "usuario": cpf}
+            print(f">> USUARIO {cpf} CADASTRADO:\nAGENCIA: {agencia} // CONTA: {conta}")
 
-  if cadastro_conta.get(cpf):
-    print(f"\n[!] CLIENTE {cpf} JÁ POSSUI UMA CONTA")
-    nome = cadastro_usuario[cpf]["nome"]
-    nascimento = cadastro_usuario[cpf]["nascimento"]
-    agencia = cadastro_conta[cpf]["agencia"]
-    conta = cadastro_conta[cpf]["conta"]
-    print(f"\n>> NOME: {nome}\n>> NASCIMENTO: {nascimento}\n>> AGÊNCIA: {agencia} // CONTA: {conta}")
-    
-    opcao = input("\n>> DESEJA CADASTRAR UMA NOVA CONTA PARA ESTE USUÁRIO? [S/N]\n>> ").upper()
-    
-    if opcao == "N":
-      return
-    
-    elif opcao == "S":
-      for valor in cadastro_conta.items():
-        lista_contas = valor[1]["conta"]
-        print(lista_contas)
-      return
-      
-  else:
-    return
-    # implementar código para adicionar conta
+        else:
+            print(f"{necessario_cadastro}")
 def listar_contas():
     
-  for conta in cadastro_conta:
-    print(f">> AGÊNCIA: {cadastro_conta[conta]['agencia']} - CONTA: {cadastro_conta[conta]['conta']}\n>> CPF: {conta}\n")
+    for conta in cadastro_conta:
+        print(f">> AGÊNCIA: {cadastro_conta[conta]['agencia']} - CONTA: {cadastro_conta[conta]['conta']}\n>> CPF: {conta}\n")
 
 while True:
     
-  opcao = input(menu).upper()
+    opcao = input(menu).upper()
 
-  if opcao == "Q":
-    print('>> SAINDO DO SISTEMA...\n')
-    break
+    if opcao == "Q":
+        print('>> SAINDO DO SISTEMA...\n')
+        break
 
-  elif opcao == "D":
-    print('=== DEPÓSITO ===\n')
-    valor_deposito = float(input(">> INFORME O VALOR DO DEPÓSITO: R$ "))
-    saldo, extrato = fdeposito(saldo, extrato, valor_deposito)
-    
-  elif opcao == "S":
-    print('=== SAQUE ===\n')
-    valor_saque = float(input(">> INFORME O VALOR DO SAQUE: R$ "))
-    saldo, extrato = fsaque(valor_saque=valor_saque, limite=limite, extrato=extrato, saldo=saldo)
+    elif opcao == "D":
+        print('=== DEPÓSITO ===\n')
+        valor_deposito = float(input(">> INFORME O VALOR DO DEPÓSITO: R$ "))
+        saldo, extrato = fdeposito(saldo, extrato, valor_deposito)
+        
+    elif opcao == "S":
+        print('=== SAQUE ===\n')
+        valor_saque = float(input(">> INFORME O VALOR DO SAQUE: R$ "))
+        saldo, extrato = fsaque(valor_saque=valor_saque, limite=limite, extrato=extrato, saldo=saldo)
 
-  elif opcao == "E":
-    print('=== EXTRATO ===\n')
-    fextrato(saldo, extrato=extrato)
+    elif opcao == "E":
+        print('=== EXTRATO ===\n')
+        fextrato(saldo, extrato=extrato)
 
-  elif opcao == "C":
-    print("=== CADASTRAR NOVO USUÁRIO ===\n")
-    cpf = input("INFORME O CPF: ")
-    cadastrar_usuario(cpf)
+    elif opcao == "C":
+        print("=== CADASTRAR NOVO USUÁRIO ===\n")
+        cpf = input("INFORME O CPF: ")
+        cadastrar_usuario(cpf)
 
-  elif opcao == "L":
-    print("=== LISTAR CLIENTES ===\n")
-    listar_usuario()
+    elif opcao == "L":
+        print("=== LISTAR CLIENTES ===\n")
+        listar_usuario()
 
-  elif opcao == "P":
-    print("=== PROCURAR POR CLIENTE ===\n")
-    procurar_usuario()
+    elif opcao == "P":
+        print("=== PROCURAR POR CLIENTE ===\n")
+        procurar_usuario()
 
-  elif opcao == "N":
-    print("=== CADASTRAR NOVA CONTA ===\n")
+    elif opcao == "N":
+        print("=== CADASTRAR NOVA CONTA ===\n")
+        cpf = input("INFORME O CPF: ")
+        cadastrar_conta(cpf)
+        
+    elif opcao == "M":
+        print("=== LISTAR CONTAS ===\n")
+        listar_contas()
 
-    for conta in cadastro_conta:
-      listar_contas = cadastro_conta[cpf]["conta"]
-      print(listar_contas)
-
-    cpf = input("INFORME O CPF: ")
-    cadastrar_conta(cpf)
-    
-  elif opcao == "M":
-    print("=== LISTAR CONTAS ===\n")
-    listar_contas()
-
-  else:
-      print("""
+    else:
+        print("""
 [!] COMANDO DESCOHECIDO
 [!] SELECIONE UMA OPÇÃO VÁLIDA""")
